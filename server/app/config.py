09d9b9b1dd81bc9cb5bdc941d6241dcf5a841fde
Matt Davies <mattdavies@monzo.com>
2026-03-11 17:26:54 +0000

    add INTERNAL_CRON_SECRET config

--- server/app/config.py
+++ server/app/config.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
     github_webhook_secret: str = ""
     seed_tenant_id: str = "00000000-0000-0000-0000-000000000001"
     seed_tenant_name: str = "dev"
+    internal_cron_secret: str = ""
 
 
 settings = Settings()
