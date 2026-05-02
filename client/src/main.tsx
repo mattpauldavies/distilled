83c14bcb033f7aa13a4aa1df47eb0f980589e781
Matt Davies <matt@malted.ai>
2026-05-02 19:29:03 +0100

    [fix] improve clerk dependency management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { ClerkProvider } from "@clerk/clerk-react"
-import { shadcn } from "@clerk/ui/themes"
+import { shadcn } from "./lib/clerkTheme"
 import "./index.css"
 import App from "./App"
 
